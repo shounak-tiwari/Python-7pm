fa2a25f2fef6389bce986bd776e869044cb582b5
Akash Tiwari <154492266+shounak-tiwari@users.noreply.github.com>
2024-09-20 20:04:15 +0530

    Add files via upload

---
+++ class 4/Cast1.py
@@ -0,0 +1,8 @@
+name = "1234"
+print("Type of name : ",type(name))
+
+print("The value of name : ",name)
+
+name = int(name)
+
+print("Type of name : ",type(name))
---
+++ class 4/input1.py
@@ -0,0 +1,15 @@
+a = int(input("Enter the value of a : "))
+
+b = int(input("Enter the value of b : "))
+
+
+print("Type  A : ",type(a))
+
+print("Type  B : ",type(b))
+print("The value of a : ",a)
+print("The value of a and b : ",a,b)
+print("The sum of a and b  : ",a+b)
+
+
+
+#defult type of input function is string 
